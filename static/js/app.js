26b2996aedc1796b38b66661175dfd4070074f9c
kellnergp <kellnergp@gmail.com>
2021-10-25 12:49:23 -0400

    initial values bubble chart added

--- static/js/app.js
+++ static/js/app.js
@@ -15,15 +15,46 @@ d3.json(url).then(function(data) {
 
     // set initial values for all panes
     function init() {
+        // select the demographic info panel
         var mdPanel = d3.select('div.panel-body');
-        
+        //store first set of metadata separately
         var mdValues = metadata[0];
+        // create counter for panel entries
+        let valNum = 0;
+        // iterate through metadata object and append paragraph tags with each k:v pair displayed
         for (let [key, value] of Object.entries(mdValues)) {
             mdEntry = mdPanel.append("p").text(`${key}: ${value}`);
+            mdEntry.attr('valNum', valNum);
+            valNum +=1;
         } 
+        // pull inital sample values into separate variable
+        initSample = samples[0];
+        console.log(initSample);
 
-        console.log(mdPanel);
-        console.log(mdValues);
+        // set paramenters for bubble chart
+        var trace1 = {
+            x: initSample.otu_ids,
+            y: initSample.sample_values,
+            text: initSample.otu_labels,
+            mode: 'markers',
+            marker: {
+              color: initSample.otu_ids,  
+              size: initSample.sample_values,
+              sizeref: 1.5,
+              sizemode: 'diameter'
+            }
+          };
+          
+          var bubbleData = [trace1];
+          // define layout for chart
+          var bubbleLayout = { 
+            xaxis: {title: {text: 'OTU ID'}},
+            showlegend: false,
+            height: 500,
+            width: 1000
+          };
+          // push bubble chart to designated div tag
+          Plotly.newPlot('bubble', bubbleData, bubbleLayout);
     }
     
     var select = d3.select("select");
