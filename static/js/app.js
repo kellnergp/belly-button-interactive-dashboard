edd6e03f6d89d809b572e0bf78dc7cb0b8245feb
kellnergp <kellnergp@gmail.com>
2021-10-24 22:36:33 -0400

    figured out how to save json arrays as variables

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,20 @@
-const dataset = d3.json('samples.json');
-//d3.json("samples.json", function(data) {console.log(data);});
+const url = "https://kellnergp.github.io/interactive-web-visualizations-challenge/samples.json"
 
+var dataset;
+var metadata;
+var names;
+var samples;
 
-console.log(dataset);
+d3.json(url).then(function(data) {
+    dataset = data;
+    metadata = data.metadata;
+    names = data.names;
+    samples = data.samples;
+    console.log(metadata, names);
+    console.log(samples);
+    
+  });
+  
+
 
 
