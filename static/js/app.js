931475499aca20d0b360f2d8f90cc7b40938ee3d
kellnergp <kellnergp@gmail.com>
2021-10-24 23:29:35 -0400

    added dropdown menu

--- static/js/app.js
+++ static/js/app.js
@@ -13,6 +13,13 @@ d3.json(url).then(function(data) {
     console.log(metadata, names);
     console.log(samples);
     
+    var select = d3.select("select");
+    for (var i = 0; i < names.length; ++i) {
+        newOption = select.append("option").text(names[i]);
+        newOption.attr('value', names[i]);
+    }
+    
+
   });
   
 
